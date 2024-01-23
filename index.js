f2ff4051185399d14c610952b3fa5c6fe066dfd6
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-01-23 23:26:24 +0400

    fixing changes

--- index.js
+++ index.js
@@ -36,17 +36,6 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-// commit
-
 app.use(cors());
 
 app.get('/', (req, res) => {
