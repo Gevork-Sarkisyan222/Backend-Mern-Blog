15e29baff9b53d92b03009e8173479f06ee53172
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2024-01-23 22:47:11 +0400

    fixing cors tools

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://chat-server-un0w.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
